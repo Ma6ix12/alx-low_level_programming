cb17247cc18570e215e873f7da29b38d482a7b19
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-08 09:21:10 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,19 @@ int main(void)
  * on the computer it is compiled and run on.
  * 
  * @return 0 Indicates a successful program execution.
- */	
-{
-  printf("Size of char: %lu bytes\n", sizeof(char));
-  printf("Size of short: %lu bytes\n", sizeof(short));
-  printf("Size of int: %lu bytes\n", sizeof(int));
-  printf("Size of long: %lu bytes\n", sizeof(long));
-  printf("Size of long long: %lu bytes\n", sizeof(long long));
-  printf("Size of float: %lu bytes\n", sizeof(float));
-  printf("Size of double: %lu bytes\n", sizeof(double));
-  printf("Size of long double: %lu bytes\n", sizeof(long double));
+ */
+}
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+ 	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+
+	#if defined(__LP64__)
+		printf("This is a 64-bit machine.\n");
+	#else
+		printf("This is a 32-bit machine.\n");
+	#endif
     
     return (0);
 }
