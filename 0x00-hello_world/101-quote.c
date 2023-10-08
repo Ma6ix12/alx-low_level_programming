8f420233378ca8f85f0dab8800bea79acfe2482d
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-08 10:48:35 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	/* Use the write function to print to standard error (file descriptor 2)*/
 	write(2, message, sizeof(message) - 1);
 	/* Return 1 to indicate program completion with an error status*/
