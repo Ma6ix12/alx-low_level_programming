b289f73fd6d0ba2f0a89ef2008e09f207222ff8e
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-08 08:23:50 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * This is the main function
+ * It prints "Programming is like building a multilingual puzzle"
+ * that is a followed by a newline character usinf the puts function
+ * 0 is returned to indicate successful execution
+ *
+ * @return 0 shows successful program execution.
+ */
 int main(void)
 	{
 	puts("Programming is like building a multilingual puzzle");
