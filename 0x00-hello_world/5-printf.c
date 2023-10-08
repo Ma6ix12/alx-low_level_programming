b01754e2945beb246015044ff70719ebfb94fc3a
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-08 08:58:49 -0700

    fox

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/*The main function of the program is the entry point of the program. It prints a specific message to the console and returns 0.*/
 int main(void)
+/*The main function of the program is the entry point of the program. It prints a specific message to the console and returns 0.*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
