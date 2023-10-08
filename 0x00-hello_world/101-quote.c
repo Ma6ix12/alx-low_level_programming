fbe59078c3e165d7d022205ed42881a43d1b54da
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-08 10:43:08 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+int main() 
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	/* Use the write function to print to standard error (file descriptor 2)*/
+	write(2, message, sizeof(message) - 1);
+	/* Return 1 to indicate program completion with an error status*/
+	return (1);
+}
+
