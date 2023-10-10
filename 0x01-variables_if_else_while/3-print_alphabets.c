6df40e12b304b0dd14a3036df936d97b0e8a0ad3
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-10 14:49:47 -0700

    fix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char letter = 'a';
+
+	/* Print lowercase alphabet*/
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+ 	letter = 'A'; /* Reset letter to 'A' for uppercase*/
+
+	/* Print uppercase alphabet*/
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n'); /* Print a new line*/
+
+	return (0);
+}
+
