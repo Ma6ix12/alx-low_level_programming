25159985ba1803bcdf9453dbf30cfb07c78ba642
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-11 11:32:59 -0700

    fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,36 +2,14 @@
 
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('\n');
-    
-	putchar('0' + 1);
-	putchar('\n');
-    
-	putchar('0' + 2);
-	putchar('\n');
-    
-	putchar('0' + 3);
-	putchar('\n');
-    
-	putchar('0' + 4);
-	putchar('\n');
-    
-	putchar('0' + 5);
-	putchar('\n');
-    
-	putchar('0' + 6);
-	putchar('\n');
-    
-	putchar('0' + 7);
-	putchar('\n');
-    
-	putchar('0' + 8);
-	putchar('\n');
-    
-	putchar('0' + 9);
-	putchar('\n');
+	putchar('0');
+	putcahr('\n');
 
+	for (int i = 1; i <= 9; i++)
+	{
+		putchar('0' + i);
+		putchar('\n')
+	}
 	return (0);
 }
 
