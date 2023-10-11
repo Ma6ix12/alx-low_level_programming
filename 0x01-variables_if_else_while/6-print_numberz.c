1b903ef02df98fc33a08da66767817cf63b40fad
Ma6ix12 <vincentmasita1@gmail.com>
2023-10-11 11:17:01 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('\n');
+    
+	putchar('0' + 1);
+	putchar('\n');
+    
+	putchar('0' + 2);
+	putchar('\n');
+    
+	putchar('0' + 3);
+	putchar('\n');
+    
+	putchar('0' + 4);
+	putchar('\n');
+    
+	putchar('0' + 5);
+	putchar('\n');
+    
+	putchar('0' + 6);
+	putchar('\n');
+    
+	putchar('0' + 7);
+	putchar('\n');
+    
+	putchar('0' + 8);
+	putchar('\n');
+    
+	putchar('0' + 9);
+	putchar('\n');
+
+	return (0);
+}
+
