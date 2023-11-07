4b4baedde432c89adb4813215b3a5046923ea1a2
Ma6ix12 <vincentmasita1@gmail.com>
2023-11-07 13:52:36 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
  * 
  * @return 0 Indicates a successful program execution.
  */
-}
+{
 	printf("Size of char: %lu byte(s)\n", sizeof(char));
  	printf("Size of int: %lu byte(s)\n", sizeof(int));
 	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
