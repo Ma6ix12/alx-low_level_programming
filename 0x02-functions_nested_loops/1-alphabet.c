273eb49f7b28194d75105788070cf1a38be3871d
Ma6ix12 <vincentmasita1@gmail.com>
2023-11-08 13:24:45 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+	/**
+	 * prints alphabet in lowercase
+	 * returns 0
+	 */
+
+	void print_alphabet(void);
+	{
+		char c;
+
+		for (c = 'a'; c < = 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+	}
