5b834fc5cde32ec7bb7940d0b1272349f4d9eb66
Ma6ix12 <vincentmasita1@gmail.com>
2024-02-22 12:06:57 -0800

    r

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
 int main(void)
-/*The main function of the program is the entry point of the program. It prints a specific message to the console and returns 0.*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
