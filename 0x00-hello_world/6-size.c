79dcbf51d7dbe6c7352e35ce77d8b9859a316884
Ma6ix12 <vincentmasita1@gmail.com>
2024-02-22 12:17:33 -0800

    u

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
-
-int main(void)
 /**
- * The main function of the program calculates and prints the sizes of different data types
- * (char, int, long, long long, float, double, and long double)
- * on the computer it is compiled and run on.
- * 
+ * main - Entry point
+ *
  * @return 0 Indicates a successful program execution.
  */
+int main(void)
 {
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
- 	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
-
-	#if defined(__LP64__)
-		printf("This is a 64-bit machine.\n");
-	#else
-		printf("This is a 32-bit machine.\n");
-	#endif
-    
-    return (0);
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+ 	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
 
